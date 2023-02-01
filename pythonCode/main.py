5e003c35cf4cb9d127080c015265df37b9fe8051
HadleighJae <hjb5471@psu.edu>
2023-02-01 12:29:43 -0500

    adding python main

--- pythonCode/main.py
+++ pythonCode/main.py
@@ -1,12 +1,13 @@
 import spacy
+# nlp = spacy.download('en_core_web_sm')
 nlp = spacy.load('en_core_web_sm')
 
-movieScripts = open("aiMovieScripts.txt", "r")
+movieScripts = open("aiMovieScripts.txt", "r", encoding='utf8')
 movieLines = movieScripts.read()
 movieStrings = str(movieLines)
 print(movieStrings) # Prints all movie lines
 
-songLyrics = open("aiSongLyrics.txt", "r")
+songLyrics = open("aiSongLyrics.txt", "r", encoding='utf8')
 songLines = songLyrics.read()
 songStrings = str(songLines)
 print(songStrings) # Prints all song lyrics
